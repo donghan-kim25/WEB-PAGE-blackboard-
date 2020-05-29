e65ddeb145bd7384dc0404d37ea5cc9a8c1a7672
donghan-kim25 <63608161+donghan-kim25@users.noreply.github.com>
2020-05-30 02:56:58 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WC</title>
+</head>
+<body>
+    <h1>D1</h1>
+    <h2>로그인을 해야합니다</h2>
+    <P>이 사이트는 소통을 위해 만들었습니다</P>
+    <form action="process_sign_in.php" method="post">
+        <p>아이디 : <input type="text" name="id" placeholder="ID"></p>
+        <p>비밀번호 : <input type="password" name="pw" placeholder="PW"></p>
+        <p><input type="submit" value="로그인하기"></p>
+    </form>
+    <p><a href="sign_up.php">회원가입</a></p>
+</body>
+</html>
