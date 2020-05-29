d9a3980927196820a44f0403d83bf23349563509
donghan-kim25 <63608161+donghan-kim25@users.noreply.github.com>
2020-05-30 03:06:05 +0900

    Create sign_up.php

---
+++ sign_up.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D1</title>
+</head>
+<body>
+    <h1>회원가입</h1>
+    <h2>회원정보 입력</h2>
+    <form action="process_sign_up.php" method="post">
+    <p>아이디 : <input type="text" name="id"></p>
+    <p>비밀번호 : <input type="password" name="pw"></p>
+    <p>닉네임 : <input type="text" name="name"></p>
+    <input type="submit" value="회원가입">
+    </form>
+</body>
+</html>
+
